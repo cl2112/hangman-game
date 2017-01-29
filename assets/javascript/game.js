68b0c16928481211a90facaed863a6b8eceb269d
cl2112 <cl2112cl2112@gmail.com>
2017-01-29 13:05:31 -0800

    added an array selector to chooseWord()

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,10 +3,16 @@ var charaChoices = ["AKUMA","RYU","CHUN-LI","CAMMY","M. BISON","KEN","VEGA","SAG
 					"FEI LONG","E. HONDA","T. HAWK","DEE JAY"]
 var moveChoices = ["SHORYUKEN","HADOUKEN","SONIC BOOM","FLASH KICK","KIKOHKEN","HUNDRED HAND SLAP","SUMO HEADBUTT","RAGING DEMON",
 					"SPINNING PILE DRIVER","TIGER SHOT","TIGER UPPERCUT","RUSH PUNCH","YOGA FLAME","CANNON SPIKE"]
-
+var totalChoices = [charaChoices, moveChoices]
+var wordChosen
 
 
 document.onkeyup = function(event) {
 	userGuess = event.key;
 	console.log(userGuess);
+}
+function chooseWord() {
+	var chosenArray = totalChoices[Math.floor(Math.random() * totalChoices.length)];
+	wordChosen = chosenArray[Math.floor(Math.random() * chosenArray.length)]
+	console.log(wordChosen);
 }
