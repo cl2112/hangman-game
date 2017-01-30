467bdddce2020376dafd53a8acb5e15e968576eb
cl2112 <cl2112cl2112@gmail.com>
2017-01-29 16:46:40 -0800

    attached a duplicate check for the on key up event

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,15 +4,29 @@ var charaChoices = ["AKUMA","RYU","CHUN-LI","CAMMY","M. BISON","KEN","VEGA","SAG
 var moveChoices = ["SHORYUKEN","HADOUKEN","SONIC BOOM","FLASH KICK","KIKOHKEN","HUNDRED HAND SLAP","SUMO HEADBUTT","RAGING DEMON",
 					"SPINNING PILE DRIVER","TIGER SHOT","TIGER UPPERCUT","RUSH PUNCH","YOGA FLAME","CANNON SPIKE"]
 var totalChoices = [charaChoices, moveChoices]
+var userInputs = []
 var wordChosen
+
 
 
 document.onkeyup = function(event) {
 	userGuess = event.key;
 	console.log(userGuess);
+	if (userInputs.indexOf(userGuess) === -1) {
+		userInputs.push(userGuess);
+	} else {
+		console.log("duplicate")
+	}
+	
 }
 function chooseWord() {
 	var chosenArray = totalChoices[Math.floor(Math.random() * totalChoices.length)];
-	wordChosen = chosenArray[Math.floor(Math.random() * chosenArray.length)]
+	wordChosen = chosenArray[Math.floor(Math.random() * chosenArray.length)];
 	console.log(wordChosen);
+}
+function arrayString() {
+	console.log(wordChosen + 2);
+}
+function userInputCheck() {
+
 }
